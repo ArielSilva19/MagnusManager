0fd689654cb21e35cf396a8a6d304ed9f7563ca4
ArielSilva19 <arielribeiro1942@gmail.com>
2018-06-06 01:28:58 -0300

    Commit 01 - por Ariel Ribeiro

--- App_Code/Classes/Cliente.cs
+++ App_Code/Classes/Cliente.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 
 /// <summary>
-/// Summary description for Fornecedor  ///   # Trampei muito!!!! kk mudança - - - Outro commit exemplo.
+/// Summary description for Fornecedor  ///   Commit novo... trampei muito!
 /// </summary>
 
 namespace MagnusManager.Classes
